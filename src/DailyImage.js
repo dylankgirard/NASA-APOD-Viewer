39ed23fda07329ce4e864b1a69fc4be7c6121caf
dylankgirard <dylankgirard@gmail.com>
2020-07-29 14:20:18 -0700

    Refactored DailyImage, changed title tab name

--- src/DailyImage.js
+++ src/DailyImage.js
@@ -3,17 +3,18 @@ import { Route, Link } from 'react-router-dom';
 
 class DailyImage extends Component {
 	render() {
+        let data = this.props.dailyData;
 		return (
 			<div className='daily-info'>
 				<h1 className='daily-banner'>Image of the Day</h1>
 				<img
 					className='daily-image'
-					src={this.props.dailyData.url}
+					src={data.url}
 					alt=''></img>
-				<h1 className='daily-banner'>{this.props.dailyData.title}</h1>
-				<p className='copyright'>{this.props.dailyData.copyright}</p>
-				<h3 className='date'>{this.props.dailyData.date}</h3>
-				<p className='explanation'>{this.props.dailyData.explanation}</p>
+				<h1 className='daily-banner'>{data.title}</h1>
+				<p className='copyright'>{data.copyright}</p>
+				<h3 className='date'>{data.date}</h3>
+				<p className='explanation'>{data.explanation}</p>
 				<Link className='next-page-link'>See Previous Image</Link>
 			</div>
 		);
--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ import './App.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
+	<Router>
+		<App />
+	</Router>,
+	document.getElementById('root')
 );
