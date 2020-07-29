96346ccc7d0a19e70dd981c8f54105ab8d4e508e
dylankgirard <dylankgirard@gmail.com>
2020-07-29 11:59:57 -0700

    Created a rough draft nav bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import Header from './Header'
 import './App.css';
 
 class App extends Component {
+	// componentDidMount() {
+	// 	const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_APOD_KEY}`;
+	// 	console.log(url);
+	// }
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			image: null,
+		};
+	}
+
+	setImage = (image) => {
+		this.setState({ image: image });
+	};
+
 	render() {
-		return (
-			<div>
-				
-			</div>
-		);
+		return <div>
+			<Header />
+		</div>;
 	}
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-
+import { Route, Link } from 'react-router-dom';
 class Header extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+	render() {
+		return (
+			<nav class='header'>
+				<img
+					className='nasa-logo-1'
+					src={require('./project-images/nasa-image-white-bg.jpg')}
+					alt=''></img>
+				<img
+					className='nasa-NASA'
+					src={require('./project-images/nasa-NASA-white-bg.jpg')}
+					alt=''></img>
+				<div>
+					<Link className='nav-links'>Archive</Link>
+					<Link className='nav-links'>About</Link>
+				</div>
+                <div className='mini-banner'>
+                    <p>National Aeronautics and Space Administration</p>
+                </div>
+			</nav>
+		);
+	}
 }
 
 export default Header;
