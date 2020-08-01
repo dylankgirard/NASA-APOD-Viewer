c99af458f7daaf0120493af4261e352b72fd946e
dylankgirard <dylankgirard@gmail.com>
2020-07-31 17:20:39 -0700

    Began building the search-by-date form

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import './SearchPage.css';
 
 class SearchPage extends Component {
 	render() {
-		return <div>
-			<h1>Search By Date</h1>
-		</div>;
+		return (
+			<div>
+				<h1>Search By Date</h1>
+				<img
+					className='search-page-logo'
+					src={require('./project-images/retro-nasa-logo-black-bg.jpg')}
+					alt=''></img>
+				<p>Search for an image between 2015-Present</p>
+				<form>
+					<div className='search-inputs'>
+						<input
+							className='search-input'
+							id='month-input'
+							placeholder='MM'
+							maxlength='2'></input>
+						<input
+							className='search-input'
+							id='day-input'
+							placeholder='DD'
+							maxlength='2'></input>
+						<input
+							className='search-input'
+							id='year-input'
+							placeholder='YYYY'
+							maxlength='4'></input>
+					</div>
+					<button className='search-button'>Check Selection</button>
+				</form>
+			</div>
+		);
 	}
 }
 
